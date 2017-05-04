bec1d1e4a62dafd3f89ea6c947355e97a941b716
tomatoandcheerio <Georxia@gmail.com>
2017-05-04 18:44:34 -0400

    Added score

--- untitled.js
+++ untitled.js
@@ -1,12 +1,8 @@
-
-var guessValues = " ";
 var arrayRightAnswer = ["HOBBIT", "HELLO", "AMERICA"];
 var arrayClue = ["Little Guys with hairy feet", "Hi", "Land of the free"];
-var rightAnswer = "";
-var totalGuessAmount = 0;
 
 var randomWordIndex = parseInt(Math.round(Math.random()*2));
-rightAnswer = arrayRightAnswer[randomWordIndex];
+var rightAnswer = arrayRightAnswer[randomWordIndex];
 
 var clue = document.getElementById("clue");
 clue.innerHTML = arrayClue[randomWordIndex];
@@ -19,7 +15,6 @@ results.innerHTML += " _ ";
 }
 
 var submitBtn = document.getElementById("submitGuess");
-
 submitBtn.addEventListener("click", function(){
 	var guessValue = document.getElementById("guessValue");
 	var recentGuess = "";
@@ -35,7 +30,8 @@ submitBtn.addEventListener("click", function(){
 
 });
 
-
+var guessValues = " ";
+var totalGuessAmount = 0;
 function checkAnswer(recentGuess){
 
 	if(guessValues.includes(recentGuess)){
@@ -43,6 +39,7 @@ function checkAnswer(recentGuess){
 	
 	} else if(rightAnswer.includes(recentGuess)){
 		guessValues += recentGuess;
+		correct();
 		showWord();
 
 	}else{
@@ -57,43 +54,42 @@ function showWord(){
 	var hidden = "";
 
 	for(var j = 0; j< rightAnswer.length; j++){
-
 		var characters = rightAnswer.charAt(j);
 		
-
 		if(guessValues.includes(characters)){
 			hidden += rightAnswer.charAt(j);
 
-
 		}else{
-
 			hidden += " _ ";
 			
 		}
 
 	}
-	
-
 	results.innerHTML= hidden;
 
 	isGameOver(totalGuessAmount, hidden);
 
 }
-
+var chancesLeft = 0;
 var gameOver = document.getElementsByClassName("game-over")[0];
 function isGameOver(number, hidden){
 	if(hidden === rightAnswer ){
 		gameOver.innerHTML = "You Win! Play Again?"
+		
 		gameOverOpition();
 
 
 	} else if(number < 5){
-		var chancesLeft =  5 - number;
+		console.log("got here");
+		 chancesLeft =  5 - number;
 
-		gameOver.innerHTML = chancesLeft;
+		gameOver.innerHTML = chancesLeft + " Chances Left";
+
+	
 
 	}else{
 		gameOver.innerHTML = "Game Over. Play Again?";
+		
 		gameOverOpition();
 
 	}
@@ -109,11 +105,40 @@ function gameOverOpition(){
 }
 
 function restartGame(){
-
 	location.reload();
 
 }
 
+var scoreContainer = document.getElementsByClassName("score")[0];
+var spinValueShow = document.createElement("div");
+var spinButton = document.getElementsByClassName("spinButton")[0];
+spinButton.addEventListener("click", spin);
+var spinValue = 0;
+var playerScore = 0;
+
+function spin(){
+	spinValueShow.innerHTML = " ";
+	spinValue = Math.round(Math.random()*60)*100;
+console.log(spinValue);
+	if(spinValue > 3000){
+		totalGuessAmount++;
+		spinValueShow.innerHTML = "You lost a Guess";
+		isGameOver(totalGuessAmount);
+		
+	
+	}else{
+		scoreContainer.append(spinValueShow);
+		spinValueShow.innerHTML = "Total Score: " + playerScore + " " + "Spin Result: " + spinValue;
+	}	
+
+}
+
+function correct(){
+	playerScore += spinValue
+	spinValueShow.innerHTML = "Total Score: " + playerScore + " " + "Spin Result: " + spinValue;
+}
+function loseAGuess(){
 
 
+}
 
